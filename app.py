bfbc5716339fb20b7ce549053a0fa3b39ad12b6e
Philipp Mayer <philipp.mayer@phape.de>
2020-06-07 14:32:15 +0200

    added index.html

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, session, request, redirect
+from flask import Flask, session, request, redirect, render_template
 from flask_session import Session
 import spotipy
 import settings
@@ -22,23 +22,19 @@ def index():
     spotify = spotipy.Spotify(auth_manager=auth_manager)
 
     if request.args.get("code"):
-        print("code:", request.args['code'])
         session['token_info'] = auth_manager.get_access_token(code=request.args['code'], as_dict=False)
-        print("token_info:", session['token_info'])
         return redirect('/')
 
     if not session.get('token_info'):
         auth_url = auth_manager.get_authorize_url()
         return f'<h2><a href="{auth_url}">Sign in</a></h2>'
-
-    return f'<h2>Hi {spotify.me()["display_name"]}, ' \
-           f'<small><a href="/sign_out">[sign out]<a/></small></h2>' \
-           f'<a href="/playlists">my playlists</a>'
+        
+    return render_template('index.html', spotify = spotify)
 
 
 @app.route('/sign_out')
 def sign_out():
-    os.remove("cache/{0}".format(session.get('uuid')))
+    os.remove('cache/{0}'.format(session.get('uuid')))
     session.clear()
     return redirect('/')
 
