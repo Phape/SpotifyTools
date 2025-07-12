08dce5ee55ea21bf5ba840544c3a39654f2e8dd4
Philipp Mayer <philipp.mayer@phape.de>
2025-07-12 22:20:57 +0200

    🔧 Fix Dart Sass filter registration and update SCSS bundle configuration

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_assets import Bundle, Environment
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
-from .sass_filter import DartSassFilter
 
 from . import settings
 
@@ -12,11 +11,11 @@ app.config.from_object(settings)
 assets = Environment(app)
 assets.url = app.static_url_path
 
-# Register custom Dart Sass filter
-assets.register('dartsass', DartSassFilter)
+# Import and register custom Dart Sass filter
+from .sass_filter import DartSassFilter
 
-# Create SCSS bundle with Dart Sass
-scss = Bundle("css/main.scss", filters="dartsass", output="css/main.css")
+# Create SCSS bundle with custom filter instance
+scss = Bundle("css/main.scss", filters=[DartSassFilter()], output="css/main.css")
 assets.register("scss_all", scss)
 
 Session(app)
