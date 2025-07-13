d6fda2f8f2bdd066f4761d01a1cf9aaf2aa995d6
Philipp Mayer <philipp.mayer@phape.de>
2025-07-13 11:05:31 +0200

    switch to sass-embedded for scss compilation

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_assets import Bundle, Environment
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
+from sass_embedded import compile_file
 
 from . import settings
 
@@ -10,8 +11,11 @@ app.config.from_object(settings)
 
 assets = Environment(app)
 assets.url = app.static_url_path
-scss = Bundle("css/main.scss", filters="pyscss", output="css/main.css")
-assets.register("scss_all", scss)
+
+# Compile SCSS file
+css = compile_file("app/static/css/main.scss", dest="app/static/css/main.css") # todo maybe deactivate source map generation in prod
+css_bundle = Bundle("css/main.scss")
+assets.register("scss_all", css_bundle)
 
 Session(app)
 
